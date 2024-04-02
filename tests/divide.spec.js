6d6247f95b6380a3275a260a7ac4e57b9dea8d38
Ulvi Azizli <ulviazizli96@gmail.com>
2024-04-02 23:33:11 +0200

    Solved Lab

--- tests/divide.spec.js
+++ tests/divide.spec.js
@@ -9,16 +9,19 @@ describe("Iteration 2", () => {
             expect(divide).toBeDefined();
         });
 
-        it("The function should take two numbers as arguments", () => {
-            expect(divide.length).toBe(2)
+        it("should take two numbers as arguments", () => {
+            expect(divide.length).toBe(2);
         });
 
-        it("The function should return the division of the two numbers", () => {
-
+        it("should return the division of the two numbers", () => {
+            expect(divide(6,3)).toEqual(2);
+            expect(divide(120,10)).toEqual(12);
         })
 
-        it("The function should return undefined if any of the arguments is not provided", () => {
-
+        it("should return undefined if any of the arguments is not provided", () => {
+            expect(divide(2)).toEqual(undefined);
+            expect(divide()).toEqual(undefined);
+            expect(divide(undefined, 5)).toEqual(undefined);
         })
 
     })    
